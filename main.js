d0a907db713670623bbac15ed33c02a4f53fd40c
Stephanie Samson <sasamson98@hotmail.com>
2019-11-01 14:17:14 -0400

    updated file with ES6 and init aos

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 // select user input
-var filter = document.querySelector("#filter");
+const filter = document.querySelector('#filter');
 
 // Filer list by the user input
-function filterNames() {
+const filterNames = () => {
     //Get value of user input
-    var filterValue = filter.value;
+    const filterValue = filter.value;
     // console.log(filterValue);
 
     // Get contact names
-    var names = document.querySelectorAll("li a");
+    const names = document.querySelectorAll('li a');
 
     // Loop over names
-    for(var i=0;i<names.length;i++) {
+    for (let name of names) {
         // compare each name with filter value
-        if(names[i].innerText.toUpperCase().includes(filterValue.toUpperCase())) {
-            names[i].parentElement.style.display = "block"
+        if (name.innerText.toUpperCase().includes(filterValue.toUpperCase())) {
+            name.parentElement.style.display = 'block';
         } else {
-            names[i].parentElement.style.display = "none"
+            name.parentElement.style.display = 'none';
         }
     }
     
-}
+};
 
 // Add event listener to user input
-filter.addEventListener("input", filterNames);
+filter.addEventListener('input', filterNames);
+
+//Initialize AOS library
+AOS.init({
+    duration: 3000
+
+});
